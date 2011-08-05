f6aa1887d70ce5367ef4e041b3829cea3a43a4a7
Colin Jones <trptcolin@gmail.com>
2011-08-04 20:58:22 -0500

    Add joodo & speclj dependencies

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject hunt_the_wumpus "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [joodo "0.6.0-SNAPSHOT"]]
+  :dev-dependencies [[speclj "1.4.0"]]
+  :test-path "spec")
