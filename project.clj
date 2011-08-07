0944c8b431e972dc9132a0b79cfbda00c3c1ab7b
Micah <micah@8thlight.com>
2011-08-06 22:28:49 -0500

    updated joodo

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject hunt_the_wumpus "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [joodo "0.6.0-SNAPSHOT"]]
+                 [joodo "0.6.1-SNAPSHOT"]]
   :dev-dependencies [[speclj "1.4.0"]
                      [clojureslim "0.0.1-SNAPSHOT"]]
   :test-path "spec")
